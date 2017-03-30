b55e4523e2c40135a1f02a871ed32c6075a99c87
Johan Gall <johan.gall@gmail.com>
2017-03-30 07:27:54 +0000

    moo

--- tools/build.py
+++ tools/build.py
@@ -576,10 +576,8 @@ class BuildObject(object):
     print('Running: %s' % (' '.join(pipes.quote(x) for x in command)))
 
     job = subprocess.Popen(command, env=env, cwd=cwd)
-    res = job.wait()
-    print('Return code: %s' % res)
 
-    return res == 0
+    return job.wait() == 0
 
 
   def execute(self, command, env=None, cwd=None):
